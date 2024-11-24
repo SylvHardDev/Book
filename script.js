a1148839e2c29c42c6ae44a44859787e8e5b680f
Mihajatiana <miandriakotonerason@gmail.com>
2024-11-24 18:06:07 +0300

    fix titre

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ gsap.to(projects, {
     anticipatePin: 1, // Fluidité de l'épingle
   },
 });
+
+const buttons = gsap.utils.toArray(".ito");
+buttons.forEach((item) => {
+  let span = item.querySelector("span");
+  let tl = gsap.timeline({ paused: true });
+
+  tl.to(span, { duration: 0.2, yPercent: -150, ease: "power2.in" });
+  tl.set(span, { yPercent: 150 });
+  tl.to(span, { duration: 0.2, yPercent: 0 });
+
+  item.addEventListener("mouseenter", () => tl.play(0));
+});
